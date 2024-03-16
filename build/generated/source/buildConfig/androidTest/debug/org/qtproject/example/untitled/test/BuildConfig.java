0677218c12243b1c9a97b6358bf0375960601d9c
xcgg75 <alsfai42mm@gmail.com>
2024-03-16 16:27:55 +0000

    first commit

---
+++ build/generated/source/buildConfig/androidTest/debug/org/qtproject/example/untitled/test/BuildConfig.java
@@ -0,0 +1,10 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.qtproject.example.untitled.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "org.qtproject.example.untitled.test";
+  public static final String BUILD_TYPE = "debug";
+}
